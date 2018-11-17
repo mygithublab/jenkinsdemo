1a27dd2f93130450f3cdc674f786e99dc18bd8cc
Trial <mygithublab@126.com>
2018-11-18 00:53:55 +0800

    20181118005355

---
+++ gitpush.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo
+git status && git add . && git commit -a -m `date +%Y%m%d%H%M%S` && git push
+
---
+++ jenkins.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar src/main/java/com/bolingcavalry/mavendockerplugindemo/MavendockerplugindemoApplication.java
