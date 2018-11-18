fc0a59b67bbbf583032c48ecff6cb8ce1af112d2
Trial <mygithublab@126.com>
2018-11-18 16:49:31 +0800

    20181118164931

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 
 docker build --rm -t test003 .
 
-#docker rm -f test003-container
+docker rm -f test003-container
 
 docker run -itd --name test003-container -p 10010:8080 -p 10011:22 \
  --privileged \
